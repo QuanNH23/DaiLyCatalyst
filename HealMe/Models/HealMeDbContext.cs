d0271241089c1de3d1c6874abdb4283d8c5c8fb5
viettran1170 <viettran1170@gmail.com>
2025-06-11 01:59:57 +0700

    FixDb

--- HealMe/Models/HealMeDbContext.cs
+++ HealMe/Models/HealMeDbContext.cs
@@ -22,8 +22,10 @@ public partial class HealMeDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-PPCF4GS\\THANH;Database=HealMeDb;user=sa;password=1;TrustServerCertificate=True");
+    {
+        var ConnectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(ConnectionString);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
